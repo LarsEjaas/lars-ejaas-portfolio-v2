a18a435a4839539b5e2639bb123dd4ba32cefc4e
Lars Ejaas <LarsEjaas@Gmail.com>
2024-09-16 21:47:33 +0200

    finish of the home page

--- src/i18n/translations/home.ts
+++ src/i18n/translations/home.ts
@@ -4,11 +4,19 @@ export const home = {
     frontend_developer_description:
       'I am passionate about web development and programming, aiming for solutions where usability goes hand in hand with performance and creativity.\nI also like to stay updated on web design trends, and when time allows, I enjoy creating my own designs.',
     profile_lars_alt: 'Profile picture of Lars Ejaas',
+    about_me: 'About',
+    about_me_title: 'Learn More About Me',
+    download_resume: 'Resumé',
+    download_resume_title: 'Download my Latest Resumé',
   },
   da: {
     frontend_developer: 'Frontend Udvikler',
     frontend_developer_description:
       'Jeg brænder for webudvikling og programmering, og jeg stræber efter løsninger, hvor brugervenlighed går hånd i hånd med performance og kreativitet.\nJeg nyder også at holde mig opdateret på webdesign trends, og når tiden tillader det, nyder jeg at lave mine egne designs.',
     profile_lars_alt: 'Profilbillede af Lars Ejaas',
+    about_me: 'Om mig',
+    about_me_title: 'Lær mig bedre at kende',
+    download_resume: 'Download CV',
+    download_resume_title: 'Download mit seneste CV',
   },
 } as const;
