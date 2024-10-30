2126fdc198cd0d1235755bb3506e8502a06bd110
Lars Ejaas <LarsEjaas@Gmail.com>
2024-10-30 22:59:57 +0100

    add 3 grid columns on work page and 2 columns on about page

--- src/i18n/translations/archive.ts
+++ src/i18n/translations/archive.ts
@@ -1,6 +1,9 @@
 export const archive = {
   en: {
     archive_title: 'Archive',
+    looking_for_old_content: 'Looking for old content?',
+    welcome_to_memory_lane:
+      'Welcome to memory lane: A cozy corner where my website’s past lives on through archived pages and retired updates.',
     old_versions_of_my_website: 'Old versions of my website',
     curious_how_my_website:
       'Curious how my website looked back in the day?\n\nLook no further - you can still find my previous version right here:',
@@ -17,9 +20,16 @@ export const archive = {
   da: {
     archive_title: 'Arkiv',
     old_versions_of_my_website: 'Ældre versioner af min hjemmeside',
+    curious_how_my_website:
+      'Nysgerrig på hvordan min hjemmeside så ud tidligere?\n\nJamen, du behøver slet ikke lede - du kan stadig finde mine forrige versioner lige her:',
     larsejaas_v1_subheadline: 'larsejaas.com version 1',
     lars_ejaas_v1_image_alt:
       'Forskellige skærmbilleder fra den originale version af larsejaas.com hjemmesiden',
+    lars_ejaas_com_v1_description:
+      'Første version of larsejaas.com bygget med React i Gatsby.js.\n',
+    old_news: 'Ældre nyheder',
+    welcome_to:
+      'Velkommen til arkivets hyggekrog, hvor gamle opdateringer udveksler historier om dengang de crashede produktionen (men overlevede til at fortælle historien videre)..."',
     lars_ejaas_v1_date: 'Feb 2021',
   },
 } as const;
