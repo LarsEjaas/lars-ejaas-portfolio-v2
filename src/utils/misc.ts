50fb60636a3bb3bc4ff8f902043be6d8f0d582a5
Lars Ejaas <LarsEjaas@Gmail.com>
2024-11-26 22:50:27 +0100

    prevent layout shifts on menu icons

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -1,3 +1,5 @@
+import type { IconImage } from '../customTypes';
+
 const validUrL =
   /https?:\/\/(www.)?[-a-zA-Z0-9@:%._+~#=]{1,256}.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/gm;
 
@@ -8,3 +10,14 @@ export const isExternalLink = (url: string) => {
 /**  Capitalize the first letter of a string */
 export const capitalize = (s: string | undefined) =>
   (s && s[0]?.toUpperCase() + s.slice(1)) || '';
+
+export const iconColors: Record<IconImage, string> = {
+  about: '#e57237',
+  alert: '#0391b1',
+  contact: '#00a0ab',
+  ejaas: '#35d2cf',
+  share: '#007d91',
+  skillset: '#a11c75',
+  work: '#0eb484',
+  archive: '#058caa',
+};
